df955570edf5774335ef5ae0abd88b630753c0cf
AaronNevalinz <aaronug421@gmail.com>
2025-05-28 18:03:34 +0300

    Rebuild assets with HTTPS base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: 'https://iot-system-core-71c7166e874b.herokuapp.com/',
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
